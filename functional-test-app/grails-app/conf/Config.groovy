286104c693bf2a5a0035f0d86f314d8765a71910
jukin-jerry <jerry@jukinmedia.com>
2017-11-05 19:20:08 -0800

    no longer have this logger, change to one we have

--- functional-test-app/grails-app/conf/Config.groovy
+++ functional-test-app/grails-app/conf/Config.groovy
@@ -55,7 +55,7 @@ log4j = {
 	error 'org.codehaus.groovy.grails',
 	      'org.springframework',
 	      'org.hibernate',
-	      'net.sf.ehcache.hibernate'
+	      'grails.plugin.cache.ehcache.hibernate.GrailsEhCacheManagerFactoryBean'
 //	debug 'org.hibernate.SQL'
 //	trace 'org.hibernate.type.descriptor.sql.BasicBinder'
 //	trace 'org.springframework.security',
